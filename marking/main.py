73c81ef7419da6a28553fef97a6f99d9f3578253
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-17 21:28:31 +0900

    Update main.py

--- marking/main.py
+++ marking/main.py
@@ -628,9 +628,6 @@ def print_possibility(file_list):
 
 	size1 = len(file_list)
 	size2 = len(error_list)
-
-	path1 = "/app/educationapp/marking/input/c_file/"
-	path2 = "/app/educationapp/marking/output/"
 	
 	if size1 == 0:
 		return
@@ -642,9 +639,7 @@ def print_possibility(file_list):
 				for j in range(size2):
 
 					if file_list[i].replace('.txt', '.c') != error_list[j]:
-						f = open(file_list[i].replace(path1, path2).replace(".c", ".txt"), 'r', encoding = "utf-8", newline = '')
-						st.write(f.readline())
-						f.close()
+						st.write(file_list[i])
 
 
 
