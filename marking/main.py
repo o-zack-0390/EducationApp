1e735ef693a52b36d4345fd9e23805e3aea8e9ae
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-17 21:07:44 +0900

    Update main.py

--- marking/main.py
+++ marking/main.py
@@ -646,6 +646,7 @@ def print_possibility(file_list):
 # 制約違反のファイルを表示する
 def print_error(error_list):
 
+	path = "/app/educationapp/marking/input/txt_file/"
 	size = len(error_list)
 	
 	if size == 0:
@@ -654,7 +655,7 @@ def print_error(error_list):
 	else:
 		with st.expander("制約違反のファイル一覧"):
 			for i in range(size):
-				f = open(path1 + '/' + error_list[i].replace(".c", ".txt"), 'r', encoding = "utf-8", newline = '')
+				f = open(path + error_list[i].replace(".c", ".txt"), 'r', encoding = "utf-8", newline = '')
 				st.write(f.readline())
 				f.close()
 
