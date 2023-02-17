8c745b94d238ea8496624671dcbed1f5151f20b3
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-17 18:08:24 +0900

    Update main.py

--- marking/main.py
+++ marking/main.py
@@ -147,6 +147,9 @@ def check_student_num():
 	size1      = len(file1_list)
 	size2      = len(file2_list)
 
+	st.write(file1_list)
+	st.write(file2_list)
+
 	if size1 != size2:
 		st.write("エラー student_c.zip と student_txt.zip 内のファイル数が一致しません")
 		exit()
